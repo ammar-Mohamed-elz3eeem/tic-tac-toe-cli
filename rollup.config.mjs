1d8e13634826fff0beb94bf927a0f65ed9f53e9b
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2024-10-30 18:38:22 +0300

    install babel to transform es6 to es5

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,22 @@
 import typescript from "@rollup/plugin-typescript";
+import nodeResolve from '@rollup/plugin-node-resolve';
+import commonjs from "@rollup/plugin-commonjs";
+import { getBabelOutputPlugin } from "@rollup/plugin-babel";
+
 
 export default {
-  input: "./src/index.ts",
-  output: {
-    dir: "./build",
-  },
-  plugins: [typescript({target: "es5"})]
+	input: "./src/index.ts",
+	output: {
+		sourcemap: true,
+		file: "./build/index.js",
+		format: "cjs",
+	},
+  plugins: [
+    typescript(),
+    nodeResolve(),
+    commonjs(),
+    getBabelOutputPlugin({
+      presets: ['@babel/preset-env']
+    })
+  ],
 };
