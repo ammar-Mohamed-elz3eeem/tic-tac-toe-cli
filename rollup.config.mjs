3428da3d86ba93b5f2c45446b6b3e2a758b6a4b7
ammar-Mohamed-elz3eeem <ammarmohamedthez@gmail.com>
2024-10-30 16:35:18 +0300

    Setup boilerplate for the typescript app

---
+++ rollup.config.mjs
@@ -0,0 +1,9 @@
+import typescript from "@rollup/plugin-typescript";
+
+export default {
+  input: "./src/index.ts",
+  output: {
+    dir: "./build",
+  },
+  plugins: [typescript({target: "es5"})]
+};
---
+++ test/index.spec.ts
@@ -0,0 +1,7 @@
+import {expect} from 'chai';
+
+describe('class Screen()', () => {
+  it("should initialize screen class", () => {
+    expect(5 + 1).to.eq(6);
+  });
+});
